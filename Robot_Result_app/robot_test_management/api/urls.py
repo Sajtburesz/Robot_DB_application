2f2df5a37af8e37dbdd5055125acca7407a82d69
Sajtburesz <93286464+Sajtburesz@users.noreply.github.com>
2023-10-16 18:17:46 +0200

    cleaining the branch

--- Robot_Result_app/robot_test_management/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from robot_test_management.api.views import UploadRobotOutputView
-
-urlpatterns = [
-    path('upload/', UploadRobotOutputView.as_view(), name='upload_robot_output'),
-]
--- Robot_Result_app/robot_test_management/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RobotTestManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'robot_test_management'
