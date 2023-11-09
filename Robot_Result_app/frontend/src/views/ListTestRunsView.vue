5ba24b5e6a22dc92638fb89206c3562e29e812aa
Bihácsi Bence <bihacsi.bence@gmail.com>
2023-11-09 23:48:39 +0100

    some styling

--- Robot_Result_app/frontend/src/views/ListTestRunsView.vue
+++ Robot_Result_app/frontend/src/views/ListTestRunsView.vue
@@ -1,47 +1,66 @@
 <template>
   <div class="container mt-5">
-    <!-- Dropdown for teams -->
-    <label for="teamSelect">Select a team</label>
-    <select class="form-select" v-model="selectedTeam"
-      @change="fetchTestRuns(`/api/v1/teams/${this.selectedTeam}/test-runs/`)" id="teamSelect">
-      <option disabled value="Please select a team">Please select a team</option>
-      <option v-for="team in teams" :key="team.id" :value="team.id">{{ team.name }}</option>
-    </select>
+    <div class="row mb-4 align-items-end">
+      <!-- Dropdown for teams -->
+      <div class="col-md-4">
+        <div class="mb-3">
+          <label for="teamSelect" class="form-label">Select a team:</label>
+          <select class="form-select form-select-sm" v-model="selectedTeam"
+            @change="fetchTestRuns(`/api/v1/teams/${this.selectedTeam}/test-runs/`)" id="teamSelect">
+            <option disabled value="" selected>Please select a team</option>
+            <option v-for="team in teams" :key="team.id" :value="team.id">{{ team.name }}</option>
+          </select>
+        </div>
+      </div>
 
-    <!-- Search Bar -->
-    <div class="mb-4"  v-if="testRuns.length != 0">
-      <div class="input-group">
-        <input type="text" v-model="searchQuery" @input="handleQueryChange" class="form-control" placeholder="Search...">
+      <!-- Search Bar -->
+      <div class="col-md-8">
+        <div class="input-group">
+          <input type="text" v-model="searchQuery" @input="handleQueryChange" class="form-control" placeholder="Search test runs...">
+          <span class="input-group-text"><i class="fas fa-search"></i></span>
+        </div>
       </div>
     </div>
 
-
     <!-- Display test runs -->
-    <div v-for="testRun in testRuns" :key="testRun.id" class="card mb-4 mt-2">
-      <router-link :to="{ name: 'TestRunView', params:{teamId: `${this.selectedTeam}`, testRunId:`${testRun.id}`} }">
+    <div v-for="testRun in testRuns" :key="testRun.id" class="card mb-3">
+      <router-link :to="{ name: 'TestRunView', params:{ teamId: `${this.selectedTeam}`, testRunId:`${testRun.id}` } }" class="text-decoration-none">
         <div class="card-body">
-          <h5 class="card-title">Attributes:</h5>
-          <div class="row">
-            <div v-for="(value, key) in testRun.attributes" :key="key" class="col-6">
-              <strong>{{ key }}</strong>: {{ value || 'N/A' }}
+          <h5 class="card-title">Test Run #{{ testRun.id }} - {{ testRun.name }}</h5>
+          <div v-if="testRun.is_public" class="badge bg-success">Public</div>
+          <div class="card-text mt-2">
+            <div class="row">
+              <div class="col-12 col-md-6" v-for="(attribute, index) in getFirstTwoAttributes(testRun.attributes)" :key="index">
+                <strong>{{ attribute.key }}:</strong> {{ attribute.value || 'N/A' }}
+              </div>
             </div>
+            <div v-if="Object.keys(testRun.attributes).length > 2" class="collapse" :id="'collapseAttributes' + testRun.id">
+              <div class="row">
+                <div class="col-12 col-md-6" v-for="(attribute, index) in getRemainingAttributes(testRun.attributes)" :key="index">
+                  <strong>{{ attribute.key }}:</strong> {{ attribute.value || 'N/A' }}
+                </div>
+              </div>
+            </div>
+            <button class="btn btn-link p-0" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapseAttributes' + testRun.id" aria-expanded="false">
+              Show more
+            </button>
           </div>
+          <p class="card-text text-muted small">Executed at: {{ new Date(testRun.executed_at).toLocaleDateString() }}</p>
         </div>
       </router-link>
     </div>
+
     <!-- Pagination -->
-    <nav>
-      <ul class="pagination" v-if="testRuns.length != 0">
+    <nav aria-label="Pagination" v-if="testRuns.length > 0">
+      <ul class="pagination justify-content-center">
         <li class="page-item" :class="{ 'disabled': !previousPageUrl }">
-          <a class="page-link btn" tabindex="-1" @click.prevent="fetchTestRuns(this.previousPageUrl)">Previous</a>
+          <a class="page-link" tabindex="-1" @click.prevent="fetchTestRuns(this.previousPageUrl)">Previous</a>
         </li>
         <li class="page-item" :class="{ 'disabled': !nextPageUrl }">
-          <a class="page-link btn" @click.prevent="fetchTestRuns(this.nextPageUrl)">Next</a>
+          <a class="page-link" @click.prevent="fetchTestRuns(this.nextPageUrl)">Next</a>
         </li>
       </ul>
     </nav>
-
-
   </div>
 </template>
 
@@ -108,6 +127,93 @@ export default {
       
       return url;
     },
-  }
+    toggleShowAllAttributes(testRun) {
+      testRun.showAllAttributes = !testRun.showAllAttributes;
+    },
+  },
+  computed: {
+    getFirstTwoAttributes() {
+      return (attributes) => {
+        return Object.entries(attributes)
+          .slice(0, 2)
+          .map(([key, value]) => ({ key, value }));
+      };
+    },
+    getRemainingAttributes() {
+      return (attributes) => {
+        return Object.entries(attributes)
+          .slice(2)
+          .map(([key, value]) => ({ key, value }));
+      };
+    },
+  },
 };
 </script>
+
+<style scoped>
+/* Styling for the component */
+
+/* Team selection dropdown and search bar */
+.form-select-sm {
+  max-width: 300px;
+}
+
+/* Test Run Cards */
+.card {
+  border: 1px solid #dcdcdc;
+  transition: transform 0.1s ease-in-out;
+}
+
+.card:hover {
+  transform: scale(1.02);
+}
+
+.card-body {
+  position: relative;
+}
+
+.card-title {
+  color: #333;
+  font-weight: bold;
+}
+
+.badge {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+}
+
+/* Pagination styles */
+.pagination .page-link {
+  color: #3a6f8f;
+  background-color: #e9f1fa;
+  border-color: #dcdcdc;
+}
+
+.pagination .page-link:hover {
+  background-color: #a3d5ff;
+  border-color: #a3d5ff;
+}
+
+.pagination .disabled .page-link {
+  color: #777;
+  background-color: #e9ecef;
+  border-color: #dcdcdc;
+  cursor: not-allowed;
+}
+
+/* Small text style for less important info */
+.small {
+  font-size: 0.8rem;
+}
+
+/* Show more button for attributes */
+.btn-link {
+  font-size: 0.8rem;
+  text-decoration: none;
+}
+
+.btn-link:hover {
+  text-decoration: underline;
+}
+</style>
